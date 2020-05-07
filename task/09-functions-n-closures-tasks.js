164104d199f34270ffed1e35e88d1ef4145f8875
Natalia Ivanova <mumplini@gmail.com>
2020-05-07 20:53:18 +0200

    feat: add function tasks implementation

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -22,157 +22,191 @@
  * @return {Function}
  *
  * @example
- *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.acos(x))
+ *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(f,g) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the math power function with the specified exponent
- *
- * @param {number} exponent
- * @return {Function}
- *
- * @example
- *   var power2 = getPowerFunction(2); // => x^2
- *   power2(2) => 4
- *   power2(4) => 16
- *
- *   var power05 = getPowerFunction(0.5); // => x^0.5
- *   power05(4) => 2
- *   power05(16) => 4
- *
- */
-function getPowerFunction(exponent) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the polynom function of one argument based on specified coefficients.
- * See: https://en.wikipedia.org/wiki/Polynomial#Definition
- *
- * @params {integer}
- * @return {Function}
- *
- * @example
- *   getPolynom(2,3,5) => y = 2*x^2 + 3*x + 5
- *   getPolynom(1,-3)  => y = x - 3
- *   getPolynom(8)     => y = 8
- *   getPolynom()      => null
- */
-function getPolynom() {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Memoizes passed function and returns function
- * which invoked first time calls the passed function and then always returns cached result.
- *
- * @params {Function} func - function to memoize
- * @return {Function} memoized function
- *
- * @example
- *   var memoizer = memoize(() => Math.random());
- *   memoizer() => some random number  (first run, evaluates the result of Math.random())
- *   memoizer() => the same random number  (second run, returns the previous cached result)
- *   ...
- *   memoizer() => the same random number  (next run, returns the previous cached result)
- */
-function memoize(func) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the function trying to call the passed function and if it throws,
- * retrying it specified number of attempts.
- *
- * @param {Function} func
- * @param {number} attempts
- * @return {Function}
- *
- * @example
- * var attempt = 0, retryer = retry(() => {
- *      if (++attempt % 2) throw new Error('test');
- *      else return attempt;
- * }, 2);
- * retryer() => 2
- */
-function retry(func, attempts) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the logging wrapper for the specified method,
- * Logger has to log the start and end of calling the specified function.
- * Logger has to log the arguments of invoked function.
- * The fromat of output log is:
- * <function name>(<arg1>, <arg2>,...,<argN>) starts
- * <function name>(<arg1>, <arg2>,...,<argN>) ends
- *
- *
- * @param {Function} func
- * @param {Function} logFunc - function to output log with single string argument
- * @return {Function}
- *
- * @example
- *
- * var cosLogger = logger(Math.cos, console.log);
- * var result = cosLogger(Math.PI));     // -1
- *
- * log from console.log:
- * cos(3.141592653589793) starts
- * cos(3.141592653589793) ends
- *
- */
-function logger(func, logFunc) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Return the function with partial applied arguments
- *
- * @param {Function} fn
- * @return {Function}
- *
- * @example
- *   var fn = function(x1,x2,x3,x4) { return  x1 + x2 + x3 + x4; };
- *   partialUsingArguments(fn, 'a')('b','c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b')('c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
- */
-function partialUsingArguments(fn) {
-    throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the id generator function that returns next integer starting from specified number every time when invoking.
- *
- * @param {Number} startFrom
- * @return {Function}
- *
- * @example
- *   var getId4 = getIdGenerator(4);
- *   var getId10 = gerIdGenerator(10);
- *   getId4() => 4
- *   getId10() => 10
- *   getId4() => 5
- *   getId4() => 6
- *   getId4() => 7
- *   getId10() => 11
- */
-function getIdGeneratorFunction(startFrom) {
-    throw new Error('Not implemented');
-}
+function getComposition(f, g) {
+    return (...args) => f(g(...args));
+  }
+  
+  
+  /**
+   * Returns the math power function with the specified exponent
+   *
+   * @param {number} exponent
+   * @return {Function}
+   *
+   * @example
+   *   const power2 = getPowerFunction(2); // => x^2
+   *   power2(2) => 4
+   *   power2(4) => 16
+   *
+   *   const power05 = getPowerFunction(0.5); // => x^0.5
+   *   power05(4) => 2
+   *   power05(16) => 4
+   *
+   */
+  function getPowerFunction(exponent) {
+    return (num) => num ** exponent;
+  }
+  
+  
+  /**
+   * Returns the polynom function of one argument based on specified coefficients.
+   * See: https://en.wikipedia.org/wiki/Polynomial#Definition
+   *
+   * @params {integer}
+   * @return {Function}
+   *
+   * @example
+   *   getPolynom(2,3,5) => y = 2*x^2 + 3*x + 5
+   *   getPolynom(1,-3)  => y = x - 3
+   *   getPolynom(8)     => y = 8
+   *   getPolynom()      => null
+   */
+  function getPolynom(...args) {
+    if (!args) return null;
+    return (x) => {
+      let y = 0;
+      for (let i = 0; i < args.length; i += 1) {
+        y += x ** (args.length - 1 - i) * args[i];
+      }
+      return y;
+    };
+  }
+  
+  
+  /**
+   * Memoizes passed function and returns function
+   * which invoked first time calls the passed function and then always returns cached result.
+   *
+   * @params {Function} func - function to memoize
+   * @return {Function} memoized function
+   *
+   * @example
+   *   const memoizer = memoize(() => Math.random());
+   *   memoizer() => some random number  (first run, evaluates the result of Math.random())
+   *   memoizer() => the same random number  (second run, returns the previous cached result)
+   *   ...
+   *   memoizer() => the same random number  (next run, returns the previous cached result)
+   */
+  function memoize(func) {
+    const calls = {};
+    return (...args) => {
+      const key = JSON.stringify(...args);
+      if (!(key in calls)) {
+        calls[key] = func(...args);
+      }
+      return calls[key];
+    };
+  }
+  
+  
+  /**
+   * Returns the function trying to call the passed function and if it throws,
+   * retrying it specified number of attempts.
+   *
+   * @param {Function} func
+   * @param {number} attempts
+   * @return {Function}
+   *
+   * @example
+   * const attempt = 0, retryer = retry(() => {
+   *      if (++attempt % 2) throw new Error('test');
+   *      else return attempt;
+   * }, 2);
+   * retryer() => 2
+   */
+  function retry(func, attempts) {
+    return () => {
+      let i = 1;
+      while (i <= attempts) {
+        try {
+          return func();
+        } catch (e) {
+          // do nothing
+        } finally {
+          i += 1;
+        }
+      }
+      return i;
+    };
+  }
+  
+  
+  /**
+   * Returns the logging wrapper for the specified method,
+   * Logger has to log the start and end of calling the specified function.
+   * Logger has to log the arguments of invoked function.
+   * The fromat of output log is:
+   * <function name>(<arg1>, <arg2>,...,<argN>) starts
+   * <function name>(<arg1>, <arg2>,...,<argN>) ends
+   *
+   *
+   * @param {Function} func
+   * @param {Function} logFunc - function to output log with single string argument
+   * @return {Function}
+   *
+   * @example
+   *
+   * const cosLogger = logger(Math.cos, console.log);
+   * const result = cosLogger(Math.PI));     // -1
+   *
+   * log from console.log:
+   * cos(3.141592653589793) starts
+   * cos(3.141592653589793) ends
+   *
+   */
+  function logger(func, logFunc) {
+    return (...args) => {
+      const params = JSON.stringify(args).replace(/^\[|\]$/g, '');
+      logFunc(`${func.name}(${params}) starts`);
+      const res = func(...args);
+      logFunc(`${func.name}(${params}) ends`);
+      return res;
+    };
+  }
+  
+  
+  /**
+   * Return the function with partial applied arguments
+   *
+   * @param {Function} fn
+   * @return {Function}
+   *
+   * @example
+   *   const fn = function(x1,x2,x3,x4) { return  x1 + x2 + x3 + x4; };
+   *   partialUsingArguments(fn, 'a')('b','c','d') => 'abcd'
+   *   partialUsingArguments(fn, 'a','b')('c','d') => 'abcd'
+   *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
+   *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
+   */
+  function partialUsingArguments(fn, ...args1) {
+    return fn.bind(this, ...args1);
+  }
+  
+  
+  /**
+   * Returns the id generator function that returns next integer starting
+   * from specified number every time when invoking.
+   *
+   * @param {Number} startFrom
+   * @return {Function}
+   *
+   * @example
+   *   const getId4 = getIdGenerator(4);
+   *   const getId10 = gerIdGenerator(10);
+   *   getId4() => 4
+   *   getId10() => 10
+   *   getId4() => 5
+   *   getId4() => 6
+   *   getId4() => 7
+   *   getId10() => 11
+   */
+  function getIdGeneratorFunction(startFrom) {
+    let i = startFrom - 1;
+    return () => { i += 1; return i; };
+  }
 
 
 module.exports = {
