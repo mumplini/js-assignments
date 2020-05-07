7eb4adb627e7222b25a49340f65fa1959c480c16
Natalia Ivanova <mumplini@gmail.com>
2020-05-07 20:32:29 +0200

    fix: another pow syntax fix

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -185,7 +185,7 @@ function getRectangleArea(width, height) {
    *   1678, 3  => 2000
    */
   function roundToPowerOfTen(num, pow) {
-    return (Math.round(num / 10 ** pow)) * (10 ** pow);
+    return (Math.round(num / Math.pow(10, pow))) * Math.pow(10, pow);
   }
   
   /**
